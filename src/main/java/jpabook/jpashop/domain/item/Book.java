6ac508800c9dac28916a0bef290dcd437ddfdec1
Sihwan-Kim <namid882@gmail.com>
2023-03-05 15:16:02 +0900

    @---ToOne fetch LAZY로 설정

--- src/main/java/jpabook/jpashop/domain/item/Book.java
+++ src/main/java/jpabook/jpashop/domain/item/Book.java
@@ -6,8 +6,8 @@ import lombok.Setter;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
 
-    @Entity
-    @DiscriminatorValue("B")
+@Entity
+@DiscriminatorValue("B")
 @Getter @Setter
 public class Book extends Item {
     private String author;
